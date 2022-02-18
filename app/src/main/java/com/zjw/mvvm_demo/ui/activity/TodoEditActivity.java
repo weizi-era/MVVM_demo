1415adfb99ab353035c0636d874001edd20f9a7e
weizi-era <408689023@qq.com>
2022-02-18 18:48:03 +0800

    第十四次提交：1.添加记事本功能 2.添加图片识别功能

--- app/src/main/java/com/zjw/mvvm_demo/ui/activity/TodoEditActivity.java
+++ app/src/main/java/com/zjw/mvvm_demo/ui/activity/TodoEditActivity.java
@@ -1,18 +1,32 @@
 package com.zjw.mvvm_demo.ui.activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.TimePickerDialog;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.DragEvent;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
+import com.github.gzuliyujiang.calendarpicker.CalendarPicker;
+import com.github.gzuliyujiang.calendarpicker.OnSingleDatePickListener;
+import com.github.gzuliyujiang.calendarpicker.core.ColorScheme;
 import com.zjw.mvvm_demo.R;
 import com.zjw.mvvm_demo.databinding.ActivityTodoEditBinding;
 import com.zjw.mvvm_demo.db.bean.Todo;
+import com.zjw.mvvm_demo.utils.EasyDate;
 import com.zjw.mvvm_demo.viewmodels.TodoEditViewModel;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Date;
 
 public class TodoEditActivity extends BaseActivity implements View.OnClickListener {
 
@@ -44,10 +58,12 @@ public class TodoEditActivity extends BaseActivity implements View.OnClickListen
         });
 
         binding.ivState.setOnClickListener(this);
-
         binding.editOk.setOnClickListener(this);
+        binding.rlTime.setOnClickListener(this);
+        binding.ivDelete.setOnClickListener(this);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -60,13 +76,49 @@ public class TodoEditActivity extends BaseActivity implements View.OnClickListen
                     isComplete = false;
                 }
                 break;
+            case R.id.rl_time:
+                selectTime();
+                break;
             case R.id.edit_ok:
                 addTodo();
                 break;
             case R.id.edit_back:
                 finish();
                 break;
+            case R.id.iv_delete:
+                binding.todoRemind.setText("添加提醒");
+                binding.ivDelete.setVisibility(View.GONE);
+                binding.ivRemind.setImageResource(R.mipmap.ic_no_remind);
+                break;
         }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void selectTime() {
+
+//        DatePicker picker = new DatePicker(this);
+//        picker.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+//            @Override
+//            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+//                binding.todoRemind.setText(year + monthOfYear + dayOfMonth);
+//                binding.ivDelete.setVisibility(View.VISIBLE);
+//            }
+//        });
+
+        CalendarPicker picker = new CalendarPicker(this);
+        picker.setColorScheme(new ColorScheme()
+                .daySelectBackgroundColor(0xFF0000FF)
+                .dayStressTextColor(0xFF0000DD));
+        picker.setOnSingleDatePickListener(new OnSingleDatePickListener() {
+            @Override
+            public void onSingleDatePicked(@NonNull @NotNull Date date) {
+                String tomorrow = EasyDate.getTomorrow(date);
+                binding.todoRemind.setText(tomorrow);
+                binding.ivDelete.setVisibility(View.VISIBLE);
+                binding.ivRemind.setImageResource(R.mipmap.ic_remind);
+            }
+        });
+        picker.show();
     }
 
     private void addTodo() {
